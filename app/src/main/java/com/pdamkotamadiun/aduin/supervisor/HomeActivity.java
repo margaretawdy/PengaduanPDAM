29101e218e7177d8c9780137dc3bef6bda0b157f
jefrydco <jefrydco@gmail.com>
2018-11-26 11:01:07 +0700

    Add logout menu.

--- app/src/main/java/com/pdamkotamadiun/aduin/supervisor/HomeActivity.java
+++ app/src/main/java/com/pdamkotamadiun/aduin/supervisor/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.pdamkotamadiun.aduin.supervisor;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.pdamkotamadiun.aduin.R;
+import com.pdamkotamadiun.aduin.global.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +16,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.menu_logout:
+                onLogout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        NavUtils.navigateUpFromSameTask(this);
+    }
+
+    private void onLogout() {
+        Intent intentToLoginActivityFromHomeActivity = new Intent(this, LoginActivity.class);
+        startActivity(intentToLoginActivityFromHomeActivity);
+        finish();
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 }
