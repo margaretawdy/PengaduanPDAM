711002332b1a3d12133d3eba5165f16017bb451f
margaretawdy <margaretawdya@gmail.com>
2018-12-12 19:53:09 +0700

    Add list and item pengaduan

---
+++ app/src/main/java/com/pdamkotamadiun/aduin/ListPengaduan.java
@@ -0,0 +1,13 @@
+package com.pdamkotamadiun.aduin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPengaduan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_pengaduan);
+    }
+}
