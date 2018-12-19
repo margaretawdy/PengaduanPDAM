8a93aa0954a5423e4d37c8e6393d01462b5a922d
margaretawdy <margaretawdya@gmail.com>
2018-12-19 18:47:34 +0700

    Add GraphOverviewActivity.java

--- app/src/main/java/com/pdamkotamadiun/aduin/activity/supervisor/GraphOverviewActivity.java
+++ app/src/main/java/com/pdamkotamadiun/aduin/activity/supervisor/GraphOverviewActivity.java
@@ -1,5 +1,6 @@
 package com.pdamkotamadiun.aduin.activity.supervisor;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 import com.pdamkotamadiun.aduin.R;
 
 import java.util.ArrayList;
@@ -22,6 +32,11 @@ public class GraphOverviewActivity extends AppCompatActivity implements AdapterV
 
         Spinner spinner = (Spinner) findViewById(R.id.activity_graph_overview_spinner_value_jenisPengaduan);
 
+        AnyChartView overViewGraphJenisKeluhan = findViewById(R.id.activity_graph_overview_chartView_keluhanMasuk);
+        overViewGraphJenisKeluhan.setProgressBar(findViewById(R.id.activity_graph_overview_progressBar_keluhanMasuk));
+
+        Pie pieJenisKeluhan = AnyChart.pie();
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -31,6 +46,13 @@ public class GraphOverviewActivity extends AppCompatActivity implements AdapterV
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        ((Pie) pieJenisKeluhan).setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(GraphOverviewActivity.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -48,6 +70,25 @@ public class GraphOverviewActivity extends AppCompatActivity implements AdapterV
 
         spinner.setAdapter(dataJenisPengaduan);
 
+        List<DataEntry> dataGrafikJenisKeluhan = new ArrayList<>();
+        dataGrafikJenisKeluhan.add(new ValueDataEntry("Buka Baru", 800));
+        dataGrafikJenisKeluhan.add(new ValueDataEntry("Tutup Rekening", 95));
+        dataGrafikJenisKeluhan.add(new ValueDataEntry("Pengaduan", 1105));
+
+        pieJenisKeluhan.data(dataGrafikJenisKeluhan);
+        pieJenisKeluhan.title("Grafik Overview Jenis Keluhan yang Masuk dalam 1 Tahun");
+        pieJenisKeluhan.labels().position("outside");
+        pieJenisKeluhan.legend().title().enabled(true);
+        pieJenisKeluhan.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pieJenisKeluhan.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        overViewGraphJenisKeluhan.setChart(pieJenisKeluhan);
     }
 
     @Override
@@ -61,5 +102,6 @@ public class GraphOverviewActivity extends AppCompatActivity implements AdapterV
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
 
